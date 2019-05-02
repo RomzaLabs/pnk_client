4a9c17ad7aaf37aeef80c3aa90f7bcb5fb32683e
Rommel Rico <me@rommelrico.com>
2019-05-01 20:48:25 -0700

    feat(header): Initial implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createBrowserHistory } from 'history';
 import { Router } from "react-router-dom";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="ui container">
       <Router history={createBrowserHistory()}>
         <div>
-          Hello World
+          <Header/>
         </div>
       </Router>
     </div>
