72a9be500a0a47c17be4cf79a5019f2236a2188d
Rommel Rico <me@rommelrico.com>
2019-05-01 20:45:29 -0700

    feat(app): Setting up history

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Router } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="ui container">
-      Hello World
+      <Router history={createBrowserHistory()}>
+        <div>
+          Hello World
+        </div>
+      </Router>
     </div>
   );
 };
