38fe60c2921be940c5bb78e4371113117b6348ee
Rommel Rico <me@rommelrico.com>
2019-05-05 20:41:46 -0700

    feat(styling): More styling for the navigation header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import { Image, Menu } from 'semantic-ui-react'
+import {Container, Image, Menu} from 'semantic-ui-react'
 
 const Header = () => {
   return (
-    <Menu>
-      <Link to="/">
-        <Image
-          src='/images/PurNKleen_logo.png'
-          size='tiny'
-        />
-      </Link>
+    <Container>
+      <Menu className="header">
+        <Link to="/">
+          <Image src='/images/PurNKleen_logo.png' size='tiny'/>
+        </Link>
 
+        <Menu.Menu>
+          <Menu.Item name='missions' active>
+            <Link to="/">Missions</Link>
+          </Menu.Item>
 
-      <Menu.Menu position={'right'}>
-        <Menu.Item name='missions' active>
-          <Link to="/">Missions</Link>
-        </Menu.Item>
+          <Menu.Item name='test1'>
+            <Link to="test1">Test 1</Link>
+          </Menu.Item>
 
-        <Menu.Item name='test1' active>
-          <Link to="test1">Test 1</Link>
-        </Menu.Item>
-
-        <Menu.Item name='test2' active>
-          <Link to="/test2">Test 2</Link>
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
+          <Menu.Item name='test2'>
+            <Link to="/test2">Test 2</Link>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </Container>
   );
 };
 
