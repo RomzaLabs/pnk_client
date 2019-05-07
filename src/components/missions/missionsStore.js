976e10b5b92b8343052bfebfdca2a3d89a0ad4d0
Rommel Rico <me@rommelrico.com>
2019-05-06 19:40:02 -0700

    feat(missionsStore): Hardcoding some missions

--- src/components/missions/missionsStore.js
+++ src/components/missions/missionsStore.js
@@ -1,10 +1,291 @@
 import {observable} from "mobx";
 
+const categories = [
+  "Bounty Hunting",
+  "Engineering",
+  "Exploration",
+  "Freelancing",
+  "Infiltration",
+  "Piracy",
+  "Resources",
+  "Scouting",
+  "Security",
+  "Smuggling",
+  "Social",
+  "Trading",
+  "Transport",
+  "Other"
+  ];
+
+const dummyMissions = [
+  {
+    id: "1",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen", "Zaden Pax"],
+    attended: ["Coff", "Zaden Pax"]
+  },
+  {
+    id: "2",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "3",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "4",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "5",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "6",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "7",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "8",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "9",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "10",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "11",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "12",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "13",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "14",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  },
+  {
+    id: "15",
+    name: "Stanton System exploration",
+    description: "We are going to explore around Stanton system for fun.",
+    discordURL: "https://discord.gg/9Xfmuw",
+    videoURL: "",
+    category: "Exploration",
+    location: "Port Olisar",
+    feature_image: "https://daszojo4xmsc6.cloudfront.net/media/event_images/beresheet2520moon2520landing_image_20190408132015.jpg",
+    date: "2019-04-11T19:00:00Z",
+    status: "Active",
+    briefing: "",
+    debriefing: "",
+    commander: "Rommel", // UserID
+    rsvpUsers: ["Axor", "Coff", "Tar Garyen"],
+    attended: ["Coff"]
+  }
+];
+
 class MissionsStore {
 
   /* Observable Properties. */
 
   @observable missions = [];
+
+  /* Constructor. */
+
+  constructor() {
+    this.missions = dummyMissions;
+  }
 
   /* Actions. */
 
