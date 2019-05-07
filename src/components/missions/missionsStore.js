c0783e940d3dab182834e2db74c2daf659623a2c
Rommel Rico <me@rommelrico.com>
2019-05-06 19:01:30 -0700

    feat(missionsStore): Adding a MobX Mission Store

---
+++ src/components/missions/missionsStore.js
@@ -0,0 +1,17 @@
+import {observable} from "mobx";
+
+class MissionsStore {
+
+  /* Observable Properties. */
+
+  @observable missions = [];
+
+  /* Actions. */
+
+  /* Computed Properties. */
+
+  /* Helpers. */
+
+}
+
+const missionsStore = new MissionsStore();
