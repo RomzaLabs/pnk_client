1044b5fa8a0bde36736341392497d183914be688
Rommel Rico <me@rommelrico.com>
2019-05-12 14:43:36 -0700

    fix(Header): Fixing the header for mobile views

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,27 @@ import './Header.css';
 import {Container, Image, Menu} from 'semantic-ui-react'
 
 const Header = () => {
+
   return (
     <Container>
-      <Menu className="header">
-        <Link to="/">
-          <Image src='/images/PurNKleen_logo.png' size='tiny'/>
-        </Link>
+      <Menu stackable>
+        <Menu.Item>
+          <Link to="/">
+            <Image src='/images/PurNKleen_logo.png' size='tiny'/>
+          </Link>
+        </Menu.Item>
 
-        <Menu.Menu>
-          <Menu.Item name='missions' active>
-            <Link to="/">Missions</Link>
-          </Menu.Item>
+        <Menu.Item name='missions' active>
+          <Link to="/">Missions</Link>
+        </Menu.Item>
 
-          <Menu.Item name='test1'>
-            <Link to="test1">Test 1</Link>
-          </Menu.Item>
+        <Menu.Item name='test1'>
+          <Link to="test1">Test 1</Link>
+        </Menu.Item>
 
-          <Menu.Item name='test2'>
-            <Link to="/test2">Test 2</Link>
-          </Menu.Item>
-        </Menu.Menu>
+        <Menu.Item name='test2'>
+          <Link to="/test2">Test 2</Link>
+        </Menu.Item>
       </Menu>
     </Container>
   );
