7479380a45208ab2c6f7f657c717eb15dda501ee
Rommel Rico <me@rommelrico.com>
2019-05-19 15:52:40 -0700

    fix(testing): Adding a beforeEach to the Header test

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 import {Menu} from 'semantic-ui-react';
 import Header from "../Header";
 
+let wrapped;
+beforeEach(() =>{
+  wrapped = shallow(<Header/>);
+});
+
 it('has a link to the homepage', () => {
-  const wrapped = shallow(<Header/>);
   const firstLinkTo = wrapped.find(Link).get(0).props.to;
   expect(firstLinkTo).toEqual("/");
 });
 
 it('has a link to the missions app', () => {
-  const wrapped = shallow(<Header/>);
   const secondLinkName = wrapped.find(Menu.Item).get(1).props.name;
   expect(secondLinkName).toEqual("missions");
 });
