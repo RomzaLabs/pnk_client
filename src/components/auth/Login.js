beab2ec22a551baa7425351065bb93570523e3e2
Rommel Rico <me@rommelrico.com>
2019-09-02 19:33:57 -0700

    :sparkles: Login: Adding form for user/pass

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,14 +1,31 @@
 import './css/Login.css';
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
-import {Container} from "semantic-ui-react";
+import {Container, Form, Button, Grid} from "semantic-ui-react";
 
 const Login = observer(class Login extends Component {
 
+  onLoginClick = () => {
+    console.log("Placeholder for handling login.");
+  };
+
   render() {
     return (
-      <Container className="content">
-        <div>Hello World</div>
+      <Container className="content full-height">
+        <Grid centered>
+          <Grid.Column>
+            <Container className="login-container">
+              <h3 className="login-headerText">
+                Log In to your account
+              </h3>
+              <Form>
+                <Form.Input label='Username' placeholder='Username' />
+                <Form.Input label='Password' type='password' />
+                <Button fluid secondary type='submit' onClick={this.onLoginClick}>Login</Button>
+              </Form>
+            </Container>
+          </Grid.Column>
+        </Grid>
       </Container>
     );
   }
