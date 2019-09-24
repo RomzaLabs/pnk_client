0c21e8f0a1348dda08131a1f31929bf7f13d8699
Rommel Rico <me@rommelrico.com>
2019-09-23 21:51:23 -0700

    :sparkles: Mission Types: Fields for the dropdown of category

--- src/components/missions/types.js
+++ src/components/missions/types.js
@@ -19,5 +19,23 @@ export const MISSION_CATEGORIES = {
   'TRD': 'Trading',
   'TRA': 'Training',
   'TRN': 'Transport',
-  'XXX': 'Top Secret'
+  'XXX': 'Transport'
 };
+
+export const MISSION_CATEGORY_OPTS = [
+  { key: 'HUN', text: 'Bounty Hunting', value: 'HUN' },
+  { key: 'ENG', text: 'Engineering', value: 'ENG' },
+  { key: 'EXP', text: 'Exploration', value: 'EXP' },
+  { key: 'FRE', text: 'Freelancing', value: 'FRE' },
+  { key: 'INF', text: 'Infiltration', value: 'INF' },
+  { key: 'PIR', text: 'Piracy', value: 'PIR' },
+  { key: 'RES', text: 'Resources', value: 'RES' },
+  { key: 'SCO', text: 'Scouting', value: 'SCO' },
+  { key: 'SEC', text: 'Security', value: 'SEC' },
+  { key: 'SMU', text: 'Smuggling', value: 'SMU' },
+  { key: 'SOC', text: 'Social', value: 'SOC' },
+  { key: 'TRD', text: 'Trading', value: 'TRD' },
+  { key: 'TRA', text: 'Training', value: 'TRA' },
+  { key: 'TRN', text: 'Transport', value: 'TRN' },
+  { key: 'XXX', text: 'Bounty Hunting', value: 'XXX' }
+];
